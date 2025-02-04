4a69441df67ec42707d0c78cefa3b684a17af2d8
CuteLoop <91436624+CuteLoop@users.noreply.github.com>
2025-02-04 14:54:39 -0700

    cython 6.5

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ import numpy as np
 
 extensions = [
     Extension(
-        name="protein_cython",
-        sources=["protein_cython.pyx"],
-        include_dirs=[np.get_include()],
-        extra_compile_args=["-O3"],
+        name="protein_cython",           # This will be the module name (protein_cython.so)
+        sources=["protein_cython.pyx"],    # The Cython source file
+        include_dirs=[np.get_include()],   # Include NumPy headers if needed
+        extra_compile_args=["-O3"],         # Optimize for speed
     )
 ]
 
 setup(
     name="protein_cython",
-    ext_modules=cythonize(extensions, compiler_directives={'language_level': "3"}),
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={'language_level': "3"}  # Use Python 3 syntax
+    ),
 )
